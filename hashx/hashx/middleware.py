bb07963d68075cd2f4bad80ad68bca4b255aee31
ramyak-mehra <rmehra_be19@thapar.edu>
2020-05-16 07:43:16 +0530

    test 2 permission system

--- hashx/hashx/middleware.py
+++ hashx/hashx/middleware.py
@@ -1,13 +1,7 @@
-from django.core.exceptions import PermissionDenied
-from .mixins import CheckUserType , CheckNodeType
-class CustomAuthorizationMiddleware(CheckUserType , object):
-    
+import graphene
+from graphql_jwt.decorators import login_required
+
+class CustomAuthorizationMiddleware(object):
     
     def resolve(self, next, root, info, **args):
-        context = info.context
-        # user_type  = CheckUserType.check_user_type(self, user = context.user)
-        CheckNodeType.check_node_type(self , root)
-        # user = info.context.user
-        # if not user.is_authenticated:
-        #     return PermissionDenied
         return next(root, info, **args)
